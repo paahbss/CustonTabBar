375c7ce612205ead4dece967cabf7cae71f182c4
Paloma Bispo <palomabispo@alu.ufc.br>
2018-11-23 10:55:16 -0300

    Initial Commit

---
+++ CustonTabBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustonTabBar
+//
+//  Created by Paloma Bispo on 23/11/18.
+//  Copyright © 2018 Paloma Bispo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
